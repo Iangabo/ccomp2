b9db4beb18b4a8278ebe55055ec5b55bdf03a2aa
ALUMNO-D <ALUMNO-D@UCSP.AULAB>
2017-08-24 11:09:16 -0500

    main

---
+++ codigos/primos.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout <<"Ingrese un numero"<< endl ;
+    int x;
+    cin >>x:
+        if(x%2!=0 && x%3!=0 && x%5!=0 && x%7!=0 )
+            cout <<"Si es primo" << endl ;
+        else
+            cout <<"No es primo" << endl ;
+    return 0
+}
+
+#include <iostream>
+using namespace std;
+int main(){
+      int a;
+      cout<<"Ingresa anio"<<endl;
+      cin>>a;
+     if(a % 4 == 0 && (a % 100 != 0 || a % 400 == 0)){
+         cout<<"El anio "<<a<<"es bisiesto ";
+     }else{
+         cout<<"El anio "<<a<<"no es bisiesto ";
+     }
+      return 0;
