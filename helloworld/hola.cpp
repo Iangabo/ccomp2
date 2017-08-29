92028e2fbf14f86e76ccac0aa667b5ec5f97f8ee
Iangabo <31105000+Iangabo@users.noreply.github.com>
2017-08-29 08:45:47 -0500

    Update hola.cpp

--- helloworld/hola.cpp
+++ helloworld/hola.cpp
@@ -1,3 +1,4 @@
+/*
 #include <iostream>
 
 using namespace std;
@@ -7,3 +8,21 @@ int main()
     cout << "Hello world!" << endl;
     return 0;
 }
+/*
+
+#include <iostream>
+int main()
+{
+for (int j=2;j<=100;j++){
+ int a=0;
+ for(int i=1;i<=100;i++)
+ {
+     if(j%i==0)
+     a++;
+ }
+ if (a==2){
+	cout << ("%d\n", j) <<endl;
+ }
+ }
+}
+
