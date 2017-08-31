9474122346d2d0a03b7c50b0d6ec68e1e6b8cb22
Iangabo <31105000+Iangabo@users.noreply.github.com>
2017-08-31 11:30:30 -0500

    Update main.cpp

--- helloworld/main.cpp
+++ helloworld/main.cpp
@@ -1,9 +1,25 @@
+//numeros ingresados y se halla menor mayor y prom
 #include <iostream>
-
 using namespace std;
-
 int main()
 {
-    cout << "Hello world!" << endl;
-    return 0;
+int n,mayor,menor,prom;
+double acumula=0.0;
+cout <<"Cuantos numeros quiere ingresar: " << endl;
+cin >> n;
+for (int cont=0,valor; n>cont; cont =cont+1){
+  cout <<"Ingrese un numero: "<< endl;
+  cin>>valor;
+  if(cont==1){
+    mayor=valor;
+    menor= valor;
+  }
+  else{
+    mayor=valor>mayor?valor:mayor;
+    menor=valor<menor?valor:menor;
+  }
 }
+cout << "el mayor es: "<<mayor<<endl;
+cout << "el menor es: "<<menor<<endl;
+prom=(mayor+menor)/2;
+cout << "el promedio es: "<<prom;
