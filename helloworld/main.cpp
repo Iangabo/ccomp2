31fe9c04be2ef0ad1b8feaaa6a72f9e244970ee7
Iangabo <31105000+Iangabo@users.noreply.github.com>
2017-09-05 09:59:04 -0500

    Update main.cpp

--- helloworld/main.cpp
+++ helloworld/main.cpp
@@ -3,27 +3,36 @@
 using namespace std;
 int main()
 {
-int n,mayor,menor,prom;
+int n,mayor,menor,valor,m;
+float prom;
 double acumula=0.0;
+m=0;
 cout <<"Cuantos numeros quiere ingresar: " << endl;
 cin >> n;
 for (int cont=0,valor; n>cont; cont =cont+1){
   cout <<"Ingrese un numero: "<< endl;
   cin>>valor;
-  if(cont==1){
+  m+=valor;
+  
+  if(cont==0){
     mayor=valor;
     menor= valor;
   }
+  
+  
   else{
     mayor=valor>mayor?valor:mayor;
     menor=valor<menor?valor:menor;
   }
+
 }
 cout << "el mayor es: "<<mayor<<endl;
 cout << "el menor es: "<<menor<<endl;
-prom=(mayor+menor)/2;
+prom=(m)/n;
 cout << "el promedio es: "<<prom;
-  
+return 0;
+}
+
 //factorial
   #include <iostream>
 using namespace std;
