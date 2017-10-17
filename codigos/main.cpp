d71c582e4a35674a70b5727ac5c758005640de77
Iangabo <31105000+Iangabo@users.noreply.github.com>
2017-10-16 21:13:28 -0500

    Update main.cpp

--- codigos/main.cpp
+++ codigos/main.cpp
@@ -1,3 +1,96 @@
+//Class
+#ifndef JUGADOR_H
+#define JUGADOR_H
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class jugador {
+  public:
+    string nombre,posicion;
+    int num, agresividad;
+
+    jugador() {
+    }
+    jugador (string x,string y, int z, int w){
+
+      nombre=x;
+      posicion=y;
+      num=z;
+      agresividad=w;
+    }
+    void setagresividad(int x){
+      agresividad=x;
+    }
+    void setnumero(int z){
+      num=z;
+    }
+    void setposicion(string w){
+      posicion=w;
+    }
+    void setnombre(string y){
+      nombre=y;
+    }
+    void print (){
+        if (agresividad>5){
+            cout <<"El jugador esta agresivo!!,dejalo jugar!"<<endl;
+        }
+        else{
+            cout <<"El jugador esta calmado, cambialo"<<endl;
+        }
+        cout <<"El nombre del jugador es: "<<nombre<<endl;
+        cout <<"La posicion del jugador es: "<<posicion<<endl;
+        cout <<"El numero del jugador es: "<<num<<endl;
+    }
+};
+
+
+#endif
+
+//main
+#include <iostream>
+#include "jugador.h"
+
+
+using namespace std;
+
+int main()
+{
+	jugador Reus("Reus", "Delantero", 11, 8);
+    Reus.print();
+    jugador Abameyang("Abameyang", "Delantero centro", 17, 8);
+    Abameyang.print();
+    jugador Dembele("Dembele", "Medio Campista", 11, 9);
+    Dembele.print();
+    jugador Kagawa("Kagawa", "Medio Campista", 23, 7);
+    Kagawa.print();
+    jugador Burki("Burki", "Portero", 1, 5);
+    Burki.print();
+    jugador Sokratis("  Sokratis", "Defensa", 25, 5);
+    Sokratis.print();
+    jugador Bartra("Bartra", "Defensa", 5, 6);
+    Bartra.print();
+    jugador Subotic("Subotic", "Defensa", 4, 4);
+    Subotic.print();
+    jugador Gotze("Gotze", "Delantero", 10, 9);
+    Gotze.print();
+    jugador Castro("Castro", "Defensa", 27, 4);
+    Castro.print();
+    jugador Rode("Rode", "Volante", 18, 5);
+    Rode.print();
+    jugador Shurrle("Shurrle", "Extremo Izquierdo", 21, 7);
+    Shurrle.print();
+
+
+
+
+
+	return 0;
+}
+
+
 Ejercicios del control
 /*1. 1.	Implementar un programa que imprima el contenido de un arreglo de dos dimensiones de forma espiral
 Por ejemplo, suponga que el arreglo contiene los siguientes valores:
