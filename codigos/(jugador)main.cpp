d16a072cc55e8208fc04a1db88845118c6e721d3
Iangabo <31105000+Iangabo@users.noreply.github.com>
2017-10-17 09:42:41 -0500

    Create (jugador)main.cpp

---
+++ codigos/(jugador)main.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include "jugador.h"
+
+
+
+using namespace std;
+
+int main()
+{
+
+    jugador Reus("Reus", "Delantero", 11, 8);
+    Reus.print();
+    jugador Abameyang("Abameyang", "Delantero centro", 17, 8);
+    Abameyang.print();
+    jugador Dembele("Dembele", "Medio Campista", 11, 9);
+    Dembele.print();
+    jugador Kagawa("Kagawa", "Medio Campista", 23, 7);
+    Kagawa.print();
+    jugador Burki("Burki", "Portero", 1, 5);
+    Burki.print();
+    jugador Sokratis("  Sokratis", "Defensa", 25, 5);
+    Sokratis.print();
+    jugador Bartra("Bartra", "Defensa", 5, 6);
+    Bartra.print();
+    jugador Subotic("Subotic", "Defensa", 4, 4);
+    Subotic.print();
+    jugador Gotze("Gotze", "Delantero", 10, 9);
+    Gotze.print();
+    jugador Castro("Castro", "Defensa", 27, 4);
+    Castro.print();
+    jugador Rode("Rode", "Volante", 18, 5);
+    Rode.print();
+    jugador Shurrle("Shurrle", "Extremo Izquierdo", 21, 7);
+    Shurrle.print();
+
+
+
+
+
+	return 0;
+}
